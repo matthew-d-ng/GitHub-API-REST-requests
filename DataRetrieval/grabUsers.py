91584d9fb50dff15fc5166a2246641d4351caee6
Matthew Ng <ngm1@tcd.ie>
2018-11-07 20:58:05 +0000

    Added function for retrieving list of contributors from a repo

--- DataRetrieval/grabUsers.py
+++ DataRetrieval/grabUsers.py
@@ -1,17 +1,33 @@
-import json
-import requests
+import operator
 from github import Github
 
-g = Github("1048acd3eab5123dd21513889f1ebff4c4e92043")
 
-for repo in g.get_user().get_repos():
+# param:   Github instance,
+#          name of repository 'user/repo_name'
+#
+# return:  List of tuples - (user, additions, deletions)
 
-    print repo.name
-    contrib_url = repo.contributors_url
-    print contrib_url
-    
-    r = requests.get(contrib_url)
-    data = r.json()
-    #print data
+def grab_lines_of_code(gh_instance, repo_name):
 
-    print data
+    base_repo = gh_instance.get_repo(repo_name)
+    user_list = dict()
+
+    print base_repo.name
+    print "running..."
+
+    for contribution in base_repo.get_stats_contributors():
+        user = contribution.author.login
+        for week in contribution.weeks:
+            if user in user_list:
+                additions = user_list[user][0]
+                deletions = user_list[user][1]
+                user_list[user] = [additions + week.a, deletions + week.d]
+            else:
+                user_list[user] = [week.a, week.d]
+
+    user_tup = list()
+    for user, code in user_list.items():
+        user_tup.append( (user, code[0], code[1]) )
+
+    return sorted( user_tup, key=operator.itemgetter(1), reverse=True )
+
