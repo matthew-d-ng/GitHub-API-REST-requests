1dd08af0d69c21b1f51256dbb5fde1ed258d8cf0
Matthew Ng <ngm1@tcd.ie>
2018-11-27 23:00:54 +0000

    Now successfully iterates over repo files

--- DataRetrieval/grabHotspots.py
+++ DataRetrieval/grabHotspots.py
@@ -1,32 +1,35 @@
 from github import Github
 
+class file:
+
+    def __init__(self, commits, lines, authors):
+        self.commits = commits
+        self.lines = lines
+        
+
 def grab_repo_files(gh_instance, repo_name):
 
     base_repo = gh_instance.get_repo(repo_name)
-    print base_repo.name
+    print base_repo.name, "\n"
 
-    # so, we need the sha of the root of the repo
-    # apparently that's not trivially included in the api
-    # we have to grab the latest commit to get it
+    commit_list = base_repo.get_commits()
+    last_commit = commit_list[0]
+    root_sha = last_commit.sha
 
-    last_commit = base_repo.get_commits.get_page(0)
-    # root_sha = last_commit.
+    tree = base_repo.get_git_tree(root_sha, recursive=True)
+    file_commits = grab_files(base_repo, tree)
 
-    # tree = base_repo.get_git_tree()
-    # print tree
-    # grab_files(base_repo, tree)
+    for file, commits in file_commits.iteritems():
+        print "%s : %d" % (file, commits)
 
 
 def grab_files(repo, tree):
 
-    leaf_list = list()
+    file_commits = dict()
     for node in tree.tree:
-        print node.path
-        if node.type == "tree":
-            sub_tree = repo.get_git_tree(node.sha, recursive=True)
-            leaf_list.append( grab_files(repo, sub_tree) )
-        elif node.type == "blob":
-            leaf_list.append( node.path )
-
-    return leaf_list
+        if node.type != "tree":
+            commits = repo.get_commits(path=node.path)
+            commit_amount = commits.totalCount
+            file_commits[node.path] = commit_amount
+    return file_commits
 
--- DataRetrieval/grabHotspotsTest.py
+++ DataRetrieval/grabHotspotsTest.py
@@ -8,7 +8,7 @@ username = raw_input("Enter github username: ")
 password = getpass.getpass("Password: ")
 g = Github(username, password)
 
-grab_repo_files(g, "matthew-d-ng/CS3012_graph")
+grab_repo_files(g, "matthew-d-ng/HaskellProblems")
 
 if g is not None:
     print "success"
